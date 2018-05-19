53f8c99a1ba128d7a61471ad3a4a3ee592c06638
szuzanna <zdrapka111@gmail.com>
2018-05-19 19:22:36 +0200

    :D

--- html_maker.py
+++ html_maker.py
@@ -2,25 +2,14 @@ from urllib.parse import urlparse
 import pycurl
 import certifi
 
+
 class HtmlMaker:
     def __init__(self, address, file):
         self.site_url = address
         self.destination_file_name = file
         self.base = self.make_it_shorter()
         self.liks_directory = self.make_courses_direcotry()
-        
-#
-#      ____________
-#     /-----------\
-#    |_____________|
-#   [-----[(O)]-[(o)]
-#    |          \ |
-#    |         o_\
-#    |    ________|
-#     \__________/
-#       |      |
-#--------\_____/---------
-#
+
     def copy_to_html(self):
         with open(self.destination_file_name, 'wb') as writer:
             c = pycurl.Curl()
