4b3d15f64f05961b0f17da3f88506b019f9f4692
stocho <34057257+stocho@users.noreply.github.com>
2018-05-19 19:28:30 +0200

    Update html_maker.py

--- html_maker.py
+++ html_maker.py
@@ -5,40 +5,40 @@ import certifi
 
 class HtmlMaker:
     def __init__(self, address, file):
-        self.site_url = address
-        self.destination_file_name = file
-        self.base = self.make_it_shorter()
-        self.liks_directory = self.make_courses_direcotry()
+        self.site_url = address;
+        self.destination_file_name = file;
+        self.base = self.make_it_shorter();
+        self.liks_directory = self.make_courses_direcotry();
 
     def copy_to_html(self):
         with open(self.destination_file_name, 'wb') as writer:
-            c = pycurl.Curl()
-            c.setopt(c.URL, self.site_url)
-            c.setopt(c.WRITEDATA, writer)
-            c.setopt(c.CAINFO, certifi.where())
-            c.perform()
-            c.close()
+            c = pycurl.Curl();
+            c.setopt(c.URL, self.site_url);
+            c.setopt(c.WRITEDATA, writer);
+            c.setopt(c.CAINFO, certifi.where());
+            c.perform();
+            c.close();
 
     def make_it_shorter(self):
-        tmp = urlparse(self.site_url)
-        return tmp.scheme + '://' + tmp.netloc
+        tmp = urlparse(self.site_url);
+        return tmp.scheme + '://' + tmp.netloc;
 
     def make_courses_direcotry(self):
-        text = open(self.destination_file_name).read()
-        text = text.split("<div")
-        linki = {}
+        text = open(self.destination_file_name).read();
+        text = text.split("<div");
+        linki = {};
         for a in text:
             if (a.__contains__("class=\"search-result__title\"><a href=")):
                 # print(a)
-                link = a[39:]
-                nazwa = link
-                link = link[:link.index("\"")]
-                nazwa = nazwa[nazwa.index(">") + 1:nazwa.index("<")]
-                linki[nazwa] = self.base + link
-        return linki
+                link = a[39:];
+                nazwa = link;
+                link = link[:link.index("\"")];
+                nazwa = nazwa[nazwa.index(">") + 1:nazwa.index("<")];
+                linki[nazwa] = self.base + link;
+        return linki;
 
-test_case = HtmlMaker('https://www.pluralsight.com/browse/it-ops', 'out.html')
-test_case.copy_to_html()
-print(test_case.base)
+test_case = HtmlMaker('https://www.pluralsight.com/browse/it-ops', 'out.html');
+test_case.copy_to_html();
+print(test_case.base);
 print(test_case.liks_directory)
 
