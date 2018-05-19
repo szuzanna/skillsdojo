5419db7d3e0165b6fddfdd6aaf4d386d265d81d3
szuzanna <zdrapka111@gmail.com>
2018-05-19 18:12:24 +0200

    nice looking html_maker

--- html_maker.py
+++ html_maker.py
@@ -1,15 +1,21 @@
 import pycurl
 import certifi
 
-url = 'https://eu.udacity.com/courses/all'
 
-with open('out.html', 'wb') as writer:
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.WRITEDATA, writer)
-    c.setopt(c.CAINFO, certifi.where())
-    c.perform()
-    c.close()
+class HtmlMaker:
+    def __init__(self, address, file):
+        self.site_url = address
+        self.destination_file_name = file
+
+    def copy_to_html(self):
+        with open(self.destination_file_name, 'wb') as writer:
+            c = pycurl.Curl()
+            c.setopt(c.URL, self.site_url)
+            c.setopt(c.WRITEDATA, writer)
+            c.setopt(c.CAINFO, certifi.where())
+            c.perform()
+            c.close()
 
 
-
+test_case = HtmlMaker('https://google.com', 'out.html')
+test_case.copy_to_html()
